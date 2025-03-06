056ba762cb39629a63bdf1a79b56df1d4192085f
Christopher Debray <christopherdebray1@gmail.com>
2025-03-06 15:02:39 +0100

    test: added user_seeder

---
+++ database/seeders/user/user_seeder.ts
@@ -0,0 +1,43 @@
+import { UserFactory } from '#database/factories/user/user_factory'
+import { UserRoles } from '#enums/user/user_roles'
+import User from '#models/user/user'
+import { BaseSeeder } from '@adonisjs/lucid/seeders'
+
+export default class extends BaseSeeder {
+  public defaultPassword = 'password'
+
+  async run() {
+    await User.createMany([
+      {
+        email: 'superadmin@gmail.com',
+        firstName: 'superadmin',
+        lastName: 'superadmin',
+        password: this.defaultPassword,
+        role: UserRoles.SUPERADMIN,
+      },
+      {
+        email: 'admin@gmail.com',
+        firstName: 'admin',
+        lastName: 'admin',
+        password: this.defaultPassword,
+        role: UserRoles.ADMIN,
+      },
+      {
+        email: 'supervisor@gmail.com',
+        firstName: 'admin',
+        lastName: 'admin',
+        password: this.defaultPassword,
+        role: UserRoles.SUPERVISOR,
+      },
+      {
+        email: 'operator@gmail.com',
+        firstName: 'operator',
+        lastName: 'operator',
+        password: this.defaultPassword,
+        role: UserRoles.OPERATOR,
+      },
+    ])
+
+    await UserFactory.createMany(5)
+  }
+}
